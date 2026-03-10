68b691aa43e4061ebcf3dd353fd824dfdae8357a
Ariel <arielmakerlab@outlook.com>
2026-03-10 15:44:16 -0300

    Se agrega el archivo sintaxis.py para mostrar los diferentes tipos de cometarios que se puede realizar

---
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/sintaxis.py
@@ -0,0 +1,9 @@
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+"""
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit.
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+"""
+
+print("Hello, world!")  # Lorem ipsum dolor sit amet

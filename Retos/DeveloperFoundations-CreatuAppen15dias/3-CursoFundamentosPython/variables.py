7fa0e6c71383c675f14aa67a3694dae3eff8e334
Ariel <arielmakerlab@outlook.com>
2026-03-11 17:01:30 -0300

    Se agrega el archivo variables.py

---
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/variables.py
@@ -0,0 +1,41 @@
+from tkinter import Variable
+
+
+x = "Esta es una variable"
+print(x)
+
+X = "Esta es otra variable"
+print(X)
+
+mivariable = "Esta es una variable con un nombre más largo"
+print(mivariable)
+mi_variable = "Esta es una variable con un nombre más largo y con guiones bajos"
+print(mi_variable)
+_mi_variable = "Esta es una variable con un nombre más largo y con guiones bajos y empieza con un guion bajo"
+print(_mi_variable)
+miVariable = "Mi variable camelCase"
+print(miVariable)  
+MiVariable = "Mi variable PascalCase"
+print(MiVariable)
+mi_variable_2 = "Esta es una variable con un número al final"
+print(mi_variable_2)
+MIVARIABLE = "Esta es una variable con mayúsculas"
+print(MIVARIABLE)
+
+# mi-variable = "Esta es una variable con un guion medio"  # Esto no es válido en Python
+# print(mi-variable)
+
+# 2mivariable = "Esta es una variable que empieza con un número"  # Esto no es válido en Python
+# print(2mivariable)
+
+# mi Variable = "Esta es una variable con un espacio"  # Esto no es válido en Python
+# print(mi Variable)
+
+camelCase = "Comiena con minúscula y cada palabra siguiente empieza con mayúscula"
+PascalCase = "Cada palabra empieza con mayúscula"
+snake_case = "Todas las letras son minúsculas y las palabras se separan con guiones bajos"
+UPPERCASE = "Todas las letras son mayúsculas"   
+print(camelCase)
+print(PascalCase)
+print(snake_case)
+print(UPPERCASE)    

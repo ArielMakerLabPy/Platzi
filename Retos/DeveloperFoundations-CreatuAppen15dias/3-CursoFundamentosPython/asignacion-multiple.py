7c8945016b99ba99db332fba4c1ee36eae55dc4e
Ariel <arielmakerlab@outlook.com>
2026-03-11 17:57:20 -0300

    Se agrega el archivo sobre el tema asignación múltiple

---
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/asignacion-multiple.py
@@ -0,0 +1,12 @@
+x,y, z = "Manzana", "Naranja", "Banana"
+
+print(x, y, z)
+
+a = b = c = "Mandarina"
+print(a, b, c)
+
+print("Mi fruta favorita es: " + x)
+print(a + " " + z)
+
+print("Hola" + "Mundo")
+

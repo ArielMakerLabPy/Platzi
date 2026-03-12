7bac32c50a93857c3c8cc183b1c74115aff4181a
Ariel <arielmakerlab@outlook.com>
2026-03-11 21:29:43 -0300

    Se agrega archivo tipos de datos en python

---
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/tipo-datos.py
@@ -0,0 +1,45 @@
+# Texto (string)
+
+comillasSimples = 'Este es un texto'
+comillasDobles = "Este es otro texto"
+comillasTriples = '''Este es un texto con comillas triples'''
+
+print(comillasSimples)
+print(comillasDobles)
+print(comillasTriples)
+
+# Números
+
+a = 1
+b = 3.14
+c = 5 + 2j
+
+print(a)
+print(b)
+print(c)
+
+# lista
+
+lista = [0, 1, 2, 3, 4, 5]
+
+print(lista)
+
+# tupla
+
+tupla = ("a", "b", "c")
+print(tupla)    
+
+# diccionario
+
+diccionario = {
+    "nombre": "Juan", 
+    "edad": 30, 
+    "ciudad": "Madrid"
+    }
+print(diccionario)
+
+# Conjunto (set)
+conjunto = {1, 1, 2, 2 , 3} # Output: {1, 2, 3}
+
+print(conjunto)
+

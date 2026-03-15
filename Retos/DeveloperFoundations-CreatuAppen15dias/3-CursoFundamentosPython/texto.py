30ef28b668557c04ba4a4aae7b29d5283eb28f30
Ariel <arielmakerlab@outlook.com>
2026-03-15 13:01:35 -0300

    Se termina el archivo sobre manejo de comillas, multiples lineas y busquedas en strings de Python en el archivo texto.py

---
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/texto.py
@@ -0,0 +1,39 @@
+from typing import TextIO
+
+
+print('Hola "Mundo"')
+
+ingles = "I'm a developer"
+
+Multilinea = """Esto
+ es
+   un
+     texto
+multilinea"""
+print(Multilinea)
+print(type(Multilinea))
+print(ingles)
+
+palabra = 'Murciélago'
+print(len(palabra))
+
+texto = 'Este curso es de fundamentos de Python'
+estaIncluida = 'Python' in texto
+noEstaIncluida = 'Javascript' not in texto
+noEstaIncluida2 = 'es' not in texto
+
+print(texto)
+print(estaIncluida)
+print(noEstaIncluida)
+print(noEstaIncluida2)
+
+mayuscula = texto.upper()
+minuscula = texto.lower()
+print(mayuscula)
+print(minuscula)
+
+espacios = '         Este es el texto'
+sinEspacios = espacios.strip()
+print(espacios)
+print(sinEspacios)
+

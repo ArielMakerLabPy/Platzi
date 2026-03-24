6f760cb6cb82e57c6946adf10d359440646b8016
Ariel Martinez <reactor@VIVOBOOK.localdomain>
2026-03-24 12:28:39 -0300

    Se crea el archivo de operadores Logicos, falta terminar

---
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/operadoresLogicos.py
@@ -0,0 +1,11 @@
+# Operadores de comparación
+
+x = 5
+y = 3
+
+print("x es igual a y:", x == y) # False
+print("x es diferente de y:", x != y) # True
+print("x es mayor que y:", x > y) # True
+print("x es menor que y:", x < y) # False
+print("x es mayor o igual que y:", x >= y) # True
+print("x es menor o igual que y:", x <= y) # False

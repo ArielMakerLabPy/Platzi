bb0c8d4dd29773ecc556f0ba27334566a3a97fa7
Reactor <arielmakerlab@outlook.com>
2026-03-24 17:24:52 -0300

    Se termina el archivo de operadores lógicos

--- Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/operadoresLogicos.py
+++ Retos/DeveloperFoundations-CreatuAppen15dias/3-CursoFundamentosPython/operadoresLogicos.py
@@ -2,6 +2,7 @@
 
 x = 5
 y = 3
+z = 5
 
 print("x es igual a y:", x == y) # False
 print("x es diferente de y:", x != y) # True
@@ -9,3 +10,16 @@ print("x es mayor que y:", x > y) # True
 print("x es menor que y:", x < y) # False
 print("x es mayor o igual que y:", x >= y) # True
 print("x es menor o igual que y:", x <= y) # False
+print("z es igual a x", x == z) # True
+
+# Operadores lógicos
+
+#       True    False
+print(x > y and y > z ) # False
+print(x > y or y > z ) # True
+
+v = True
+f = False
+
+print(not(v))
+print(not(f))
